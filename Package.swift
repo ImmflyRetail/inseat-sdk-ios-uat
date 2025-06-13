bc7204b9eca56dd0ea30678a926891e1e6c1f741
rpinto-immfly <164333537+rpinto-immfly@users.noreply.github.com>
2025-06-13 16:34:06 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Inseat",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "Inseat", 
+            targets: [
+                "InseatWrapper"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/getditto/DittoSwiftPackage", exact: "4.10.2")
+    ],
+    targets: [
+        .target(
+            name: "InseatWrapper",
+            dependencies: [
+                .target(name: "InseatFramework"),
+                .product(name: "DittoSwift", package: "DittoSwiftPackage")
+            ]
+        ),
+        .binaryTarget(
+            name: "InseatFramework",
+            url: "https://app-cdn.immflyretail.link/inseat-ios-sdk/0.1.3/Inseat.xcframework.zip",
+            checksum: "2cc88a1774a345f8c2f6f048ed03bc06ec263ecd5c2a2a013a6c2801b86b4851"
+        )
+    ]
+)
