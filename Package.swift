fd3fb828dc2fab766e727b5a9f9f91a8bf26a575
Version agent <itretail@immfly.com>
2025-09-17 09:17:55 +0000

    Changes for version 0.1.8

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "InseatFramework",
-            url: "https://app-cdn.immflyretail.link/inseat-ios-sdk/0.1.7/Inseat.xcframework.zip",
-            checksum: "e805b1bcbf2eb6177b05b515141c4923a56ac50d4928a580ac572e070dbe0524"
+            url: "https://app-cdn.immflyretail.link/inseat-ios-sdk/0.1.8/Inseat.xcframework.zip",
+            checksum: "0f170df636ce9f657b059274753e52166d1dbe62d741401dcffa3bf039255e55"
         )
     ]
 )
